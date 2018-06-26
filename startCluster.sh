2722ad0ce9310b4b20d68801de71fcaba173ec37
Steve Farrell <sparticlesteve@gmail.com>
2018-06-26 09:18:53 -0700

    clean up startCluster script

--- startCluster.sh
+++ startCluster.sh
@@ -10,13 +10,7 @@ clusterID=cori_${SLURM_JOB_ID}
  
 echo "Launching controller"
 ipcontroller --ip="$headIP" --cluster-id=$clusterID &
-#--profile="${profile}"
-#--log-to-file
-#--nodb
 sleep 20
  
 echo "Launching engines"
 srun ipengine --cluster-id=$clusterID
-#--profile="$profile"
-#--location=$(hostname)
-#--log-to-file
