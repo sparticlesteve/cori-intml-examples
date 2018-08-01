d58c7a6bf41005df6214613d52223ab4d21b89b3
Steve Farrell <sparticlesteve@gmail.com>
2018-07-31 22:04:11 -0700

    cleanup environment settings in scripts

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@
 conda activate /global/common/software/dasrepo/JupyterDL
 
 # Set some threading environment variables
+export KMP_AFFINITY="granularity=fine,compact,1,0"
+export KMP_BLOCKTIME=1
 export NUM_INTER_THREADS=2
 export NUM_INTRA_THREADS=32
-export OMP_NUM_THREADS=32
+export OMP_NUM_THREADS=$NUM_INTRA_THREADS
