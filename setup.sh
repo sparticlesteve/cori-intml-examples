cff338c8d269cbf5df6bfc0ed6e4db097a15f4ba
Steve Farrell <sparticlesteve@gmail.com>
2019-10-24 21:19:31 -0700

    move to tf 1.13.1

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # Example environment setup script for jupyter deep learning on Cori
 
 # Setup the Cori software
-module load tensorflow/intel-1.9.0-py36
+module load tensorflow/intel-1.13.1-py36
 
 # Set some threading environment variables
 export KMP_AFFINITY="granularity=fine,compact,1,0"
