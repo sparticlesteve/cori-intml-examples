a2f61fbaf28fa3fbdd89fb9edc1dde919c3990ff
Steve Farrell <sparticlesteve@gmail.com>
2019-10-24 21:19:16 -0700

    remove unused mlextras.build_and_train

--- mlextras.py
+++ mlextras.py
@@ -41,26 +41,3 @@ def configure_session():
         intra_op_parallelism_threads=n_intra_threads
     )
     return tf.Session(config=config)
-
-
-def build_and_train(x_train, y_train, valid_frac, batch_size, n_epochs,
-                    h1, h2, h3, dropout, optimizer, verbose=0, nthreads=1,
-                    checkpoint_file=None, callbacks=[]):
-    """Run training for one set of hyper-parameters."""
-
-    # Build the model
-    model = build_model(h1=h1, h2=h2, h3=h3,
-                        dropout=dropout, optimizer=optimizer)
-
-    ipp_logger = IPyParallelLogger()
-    callbacks.append(ipp_logger)
-
-    if checkpoint_file is not None:
-        callbacks.append(keras.callbacks.ModelCheckpoint(checkpoint_file))
-
-    # Train the model
-    history = model.fit(x_train, y_train,
-                        validation_split=valid_frac,
-                        batch_size=batch_size, epochs=n_epochs,
-                        verbose=verbose, callbacks=callbacks)
-    return history.history
