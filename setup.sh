887cc90fb47628eba939c6e95137f5f029cf71af
Steve Farrell <sparticlesteve@gmail.com>
2019-11-05 16:59:11 -0800

    moved to latest cray-hpo installation

--- setup.sh
+++ setup.sh
@@ -3,12 +3,12 @@
 # Setup the Cori software
 module load tensorflow/intel-1.13.1-py36
 
+# Cray HPO library
+module load cray-hpo
+
 # Set some threading environment variables
 export KMP_AFFINITY="granularity=fine,compact,1,0"
 export KMP_BLOCKTIME=1
 export NUM_INTER_THREADS=2
 export NUM_INTRA_THREADS=32
 export OMP_NUM_THREADS=$NUM_INTRA_THREADS
-
-# Cray HPO library
-export PYTHONPATH=/global/cscratch1/sd/sfarrell/cray-hpo/software
